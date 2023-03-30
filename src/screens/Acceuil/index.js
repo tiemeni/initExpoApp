2b4fa7ae5594da89858e151b27dfac721398f8c5
dongmodonald95 <dongmodonald95@gmail.com>
2023-03-30 13:09:24 +0100

    interface d'accueil sans dynamisation

--- src/screens/Acceuil/index.js
+++ src/screens/Acceuil/index.js
@@ -1,21 +1,53 @@
-import { Text, Image } from 'react-native'
 import React from 'react'
 import { CustomHeader } from '../MesRdv'
 import { PROFILE } from '../../constants/screens'
-import heartBit from "../../assets/img/heart-beat.png"
-import { View, VStack } from 'native-base'
+import { Box, Input, ScrollView, View, HStack, Text, VStack } from 'native-base'
+import MedCard from '../../components/MedCard';
+import styles from './style';
+import { specialites, practiciens} from '../../utils/helper';
 
 const Acceuil = ({ navigation }) => {
 
   return (
     <View flex={1}>
       <CustomHeader navigation={navigation} screen={PROFILE} />
-      <VStack justifyContent={'center'} flex={2}>
-        <VStack alignItems={'center'}>
-          <Image source={heartBit} style={{ height: 70, width: 70 }} />
-          <Text style={{ color: "#999999" }}>Aucune donnée</Text>
+      <ScrollView showsVerticalScrollIndicator={false} padding={5}>
+        <Box>
+          <Input
+            variant={"unstyled"}
+            onChangeText={(v) => handleChange('securityCode', v)}
+            style={styles.input}
+            size={'md'}
+            placeholder="Rechercher un docteur ou une spécialité" />
+        </Box>
+        <Box mb={7} style={styles.tipBox}></Box>
+
+        <VStack mb={7}>
+          <HStack mb={2} justifyContent={'space-between'}>
+            <Text style={styles.sectionTitle}>Nos spécialités</Text>
+            <Text style={styles.seeAll}>Tout voir</Text>
+          </HStack>
+          <HStack style={styles.specialityBox}>
+            {specialites.map((specialite) => (
+              <Box key={specialite.id} style={styles.speciality}>
+                <Text style={styles.specialityText}>{specialite.value}</Text>
+              </Box>
+            ))}
+          </HStack>
         </VStack>
-      </VStack>
+
+        <VStack>
+          <HStack mb={4} justifyContent={'space-between'}>
+            <Text style={styles.sectionTitle}>Médecins populaires</Text>
+            <Text style={styles.seeAll}>Tout voir</Text>
+          </HStack>
+          <VStack flex={1} mb={10}>
+            <ScrollView showsHorizontalScrollIndicator={false} horizontal={true}>
+              {practiciens.map((praticien) => <MedCard key={praticien.id} praticien={praticien} />)}
+            </ScrollView>
+          </VStack>
+        </VStack>
+      </ScrollView>
     </View>
   )
 }
