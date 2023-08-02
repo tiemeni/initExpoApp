069ba6453914c0b9aee9ba9f929e8e226fc4fce9
aicscloud <aicscloud@gmail.com>
2023-08-02 18:18:07 +0200

    commit du 02/07/2023

--- src/screens/MesRdv/index.js
+++ src/screens/MesRdv/index.js
@@ -1,218 +1,243 @@
-import React, { useEffect, useState } from 'react'
-import { Avatar, Box, HStack, ScrollView, View, VStack } from 'native-base'
-import { ActivityIndicator, Image, StyleSheet, Text } from 'react-native'
-import colors from '../../constants/colours'
-import plusBlack from "../../assets/img/plus_black.png"
-import Rdv from '../../components/Rdv'
-import { Entypo } from '@expo/vector-icons';
-import Pressable from 'react-native/Libraries/Components/Pressable/Pressable'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import * as SCREENS from '../../constants/screens'
-import styles from './style'
+import React, { useEffect, useState } from "react";
+import {
+  Avatar,
+  Box,
+  HStack,
+  ScrollView,
+  View,
+  VStack,
+  Skeleton,
+  Center,
+} from "native-base";
+import { ActivityIndicator, Image, StyleSheet, Text } from "react-native";
+import colors from "../../constants/colours";
+import plusBlack from "../../assets/img/plus_black.png";
+import Rdv from "../../components/Rdv";
+import { Entypo } from "@expo/vector-icons";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import * as SCREENS from "../../constants/screens";
+import styles from "./style";
+import { Skelette } from "./squelette";
 
-export const IsLoadingComponent = () => {
-    return (
-        <View style={{
-            padding: 3,
-            alignItems: 'center',
-            width: '100%',
-        }}>
-            <View>
-                <ActivityIndicator
-                    color={colors.primary}
-                    size={30}
-                    style={{
-                        backgroundColor: "white",
-                        borderRadius: 100,
-                        padding: 5,
-                        elevation: 5
-                    }} />
-            </View>
-        </View>
-    )
-}
 
 export const CustomHeader = ({ navigation, mb, screen }) => {
-    return (
-        <HStack
-            justifyContent={"space-between"}
-            padding={2}
-            alignItems={'center'}
-            backgroundColor={'white'}
-            style={{
-                shadowColor: "#000",
-                shadowOffset: {
-                    width: 0,
-                    height: 1,
-                },
-                shadowOpacity: 0.1,
-                shadowRadius: 3.84,
-                elevation: 1
+  return (
+    <HStack
+      justifyContent={"space-between"}
+      padding={2}
+      alignItems={"center"}
+      backgroundColor={"white"}
+      style={{
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 1,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 3.84,
+        elevation: 1,
+      }}
+      mb={mb}
+    >
+      <HStack alignItems={"center"}>
+        <Pressable onPress={() => navigation.navigate(screen)}>
+          <Avatar
+            bg={colors.primary}
+            width={37}
+            height={37}
+            source={{
+              uri: null,
             }}
-            mb={mb}>
-            <HStack
-                alignItems={"center"}>
-                <Pressable
-                    onPress={() => navigation.navigate(screen)}>
-                    <Avatar
-                        bg={colors.primary}
-                        width={37}
-                        height={37}
-                        source={{
-                            uri: null
-                        }}></Avatar>
-                </Pressable>
-                <Text style={{ marginLeft: 15, fontSize: 18 }}>John Doe</Text>
-            </HStack>
-            <TouchableOpacity onPress={() => navigation.navigate(SCREENS.MAKE_APPOINTMENT_SCREEN)}>
-             <Box style={styles.boxGoogle} width={50} rounded={50} shadow={2}>
-                 <Entypo name="plus" size={29} color={colors.white} />
-             </Box>
-            </TouchableOpacity>
-        </HStack>
-    )
-}
+          ></Avatar>
+        </Pressable>
+        <Text style={{ marginLeft: 15, fontSize: 18 }}>John Doe</Text>
+      </HStack>
+      <TouchableOpacity
+        onPress={() => navigation.navigate(SCREENS.MAKE_APPOINTMENT_SCREEN)}
+      >
+        <Box style={styles.boxGoogle} width={50} rounded={50} shadow={2}>
+          <Entypo name="plus" size={29} color={colors.white} />
+        </Box>
+      </TouchableOpacity>
+    </HStack>
+  );
+};
 
 export default function MesRdv({ navigation }) {
+  const [actualState, setActualState] = useState(1);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 10000);
+  });
 
-    const [actualState, setActualState] = useState(1)
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false)
-        }, 1000)
-    })
-
-    return (
-        <View flex={1}>
-            <CustomHeader navigation={navigation} mb={5} screen={SCREENS.PROFILE} />
-            <Box
-                width={"100%"}
-                justifyContent={'center'}
-                alignItems={'center'}
-                height={45}
-                mb={5}>
-                <HStack
-                    width={"95%"}
-                    alignItems={'center'}
-                    height={'95%'}
-                    backgroundColor={'#EEEFF3'}
-                    borderRadius={10}>
-                    <View
-                        display={"flex"}
-                        flexDirection={'row'}
-                        justifyContent={'center'}
-                        borderRightWidth={1}
-                        borderRightColor={'#DADADA'}
-                        height={'90%'}
-                        width={"33%"}>
-                        <Pressable
-                            onPress={() => {
-                                setActualState(1)
-                                setLoading(true)
-                                setTimeout(() => {
-                                    setLoading(false)
-                                }, 1000)
-                            }}
-                            display={"flex"}
-                            flexDirection={"row"}
-                            justifyContent={'center'}
-                            alignItems={'center'}
-                            backgroundColor={actualState === 1 ? "#04B7C9" : "#EEEFF3"}
-                            height={"100%"}
-                            width={'90%'}
-                            borderRadius={10}
-                        >
-                            <TouchableOpacity>
-                                <Text style={{ fontSize: 16, color: actualState === 1 ? "white" : "#909090" }}>A venir</Text>
-                            </TouchableOpacity>
-                        </Pressable>
-                    </View>
-                    <View
-                        display={"flex"}
-                        flexDirection={'row'}
-                        justifyContent={'center'}
-                        borderRightWidth={1}
-                        borderRightColor={'#DADADA'}
-                        height={'90%'}
-                        width={"34%"}>
-                        <Pressable
-                            onPress={() => {
-                                setActualState(2)
-                            }}
-                            display={"flex"}
-                            flexDirection={"row"}
-                            justifyContent={'center'}
-                            alignItems={'center'}
-                            backgroundColor={actualState === 2 ? "#04B7C9" : "#EEEFF3"}
-                            height={"100%"}
-                            width={'90%'}
-                            borderRadius={10}>
-                            <TouchableOpacity>
-                                <Text style={{ fontSize: 16, color: actualState === 2 ? "white" : "#909090" }}>Terminé</Text>
-                            </TouchableOpacity>
-                        </Pressable>
-                    </View>
-                    <View
-                        height={'90%'}
-                        width={"33%"}
-                        display={"flex"}
-                        flexDirection={'row'}
-                        borderRightColor={'#DADADA'}
-                        justifyContent={'center'}>
-                        <Pressable
-                            onPress={() => setActualState(3)}
-                            display={"flex"}
-                            flexDirection={"row"}
-                            justifyContent={'center'}
-                            alignItems={'center'}
-                            backgroundColor={actualState === 3 ? "#04B7C9" : "#EEEFF3"}
-                            height={"100%"}
-                            width={'90%'}
-                            borderRadius={10}>
-                            <TouchableOpacity>
-                                <Text style={{ fontSize: 16, color: actualState === 3 ? "white" : "#909090" }}>Annulé</Text>
-                            </TouchableOpacity>
-                        </Pressable>
-                    </View>
-                </HStack>
-            </Box>
-            <ScrollView
-                overScrollMode='never'
+  return (
+    <View flex={1}>
+      <CustomHeader navigation={navigation} mb={5} screen={SCREENS.PROFILE} />
+      <Box
+        width={"100%"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        height={45}
+        mb={5}
+      >
+        <HStack
+          width={"95%"}
+          alignItems={"center"}
+          height={"95%"}
+          backgroundColor={"#EEEFF3"}
+          borderRadius={10}
+        >
+          <View
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"center"}
+            borderRightWidth={1}
+            borderRightColor={"#DADADA"}
+            height={"90%"}
+            width={"33%"}
+          >
+            <Pressable
+              onPress={() => {
+                setActualState(1);
+                setLoading(true);
+                setTimeout(() => {
+                  setLoading(false);
+                }, 1000);
+              }}
+              display={"flex"}
+              flexDirection={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              backgroundColor={actualState === 1 ? "#04B7C9" : "#EEEFF3"}
+              height={"100%"}
+              width={"90%"}
+              borderRadius={10}
             >
-                {actualState === 1 && !loading ?
-                    <VStack justifyContent={"center"} alignItems={'center'}>
-                        {[1, 2, 3, 4, 5, 6].map((_e, i) => (
-                            <View
-                                key={i}
-                                height={182}
-                                width={340}
-                                borderRadius={10}
-                                padding={3}
-                                mb={2}
-                                backgroundColor={'white'}
-                                style={{
-                                    shadowColor: "#000",
-                                    shadowOffset: {
-                                        width: 0,
-                                        height: 1,
-                                    },
-                                    shadowOpacity: 0.1,
-                                    shadowRadius: 3.84,
-                                    elevation: 1
-                                }}
-                            >
-                                <Rdv />
-                            </View>)
-                        )}
-                    </VStack> :
-                    actualState !== 1 && !loading ?
-                        <VStack>
-                        </VStack> :
-                        <VStack>
-                            <IsLoadingComponent />
-                        </VStack>}
-            </ScrollView>
-        </View>
-    )
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    color: actualState === 1 ? "white" : "#909090",
+                  }}
+                >
+                  A venir
+                </Text>
+              </TouchableOpacity>
+            </Pressable>
+          </View>
+          <View
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"center"}
+            borderRightWidth={1}
+            borderRightColor={"#DADADA"}
+            height={"90%"}
+            width={"34%"}
+          >
+            <Pressable
+              onPress={() => {
+                setActualState(2);
+              }}
+              display={"flex"}
+              flexDirection={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              backgroundColor={actualState === 2 ? "#04B7C9" : "#EEEFF3"}
+              height={"100%"}
+              width={"90%"}
+              borderRadius={10}
+            >
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    color: actualState === 2 ? "white" : "#909090",
+                  }}
+                >
+                  Terminé
+                </Text>
+              </TouchableOpacity>
+            </Pressable>
+          </View>
+          <View
+            height={"90%"}
+            width={"33%"}
+            display={"flex"}
+            flexDirection={"row"}
+            borderRightColor={"#DADADA"}
+            justifyContent={"center"}
+          >
+            <Pressable
+              onPress={() => setActualState(3)}
+              display={"flex"}
+              flexDirection={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              backgroundColor={actualState === 3 ? "#04B7C9" : "#EEEFF3"}
+              height={"100%"}
+              width={"90%"}
+              borderRadius={10}
+            >
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    color: actualState === 3 ? "white" : "#909090",
+                  }}
+                >
+                  Annulé
+                </Text>
+              </TouchableOpacity>
+            </Pressable>
+          </View>
+        </HStack>
+      </Box>
+      <ScrollView overScrollMode="never">
+        {actualState === 1 && !loading ? (
+          <VStack justifyContent={"center"} alignItems={"center"}>
+            {[1, 2, 3, 4, 5, 6].map((_e, i) => (
+              <View
+                key={i}
+                height={182}
+                width={340}
+                borderRadius={10}
+                padding={3}
+                mb={2}
+                backgroundColor={"white"}
+                style={{
+                  shadowColor: "#000",
+                  shadowOffset: {
+                    width: 0,
+                    height: 1,
+                  },
+                  shadowOpacity: 0.1,
+                  shadowRadius: 3.84,
+                  elevation: 1,
+                }}
+              >
+                <Rdv />
+              </View>
+            ))}
+          </VStack>
+        ) : actualState !== 1 && !loading ? (
+          <VStack></VStack>
+        ) : (
+          <VStack padding={4} space={1}> 
+          <VStack>
+            <Skelette />
+          </VStack>
+          <VStack>
+            <Skelette />
+          </VStack>
+          <VStack>
+            <Skelette />
+          </VStack>
+          </VStack> 
+        )}
+      </ScrollView>
+    </View>
+  );
 }
---
+++ src/screens/MesRdv/squelette.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import {
+  Avatar,
+  Circle,
+  HStack,
+  VStack,
+  Skeleton,
+  Divider,
+  Center,
+} from "native-base";
+import colors from "../../constants/colours";
+
+export const Skelette = () => {
+  return (
+    <View
+      style={{
+        height: 180,
+        width: "100%",
+        padding:5,
+        marginTop: 0,
+        borderRadius:15,
+        alignItems: "center",
+        backgroundColor:'white'
+      }}
+    >
+      <HStack mt={3} marginBottom={2} justifyContent={"space-between"}>
+        <VStack flex={1} width={'100%'} space={3}>
+            <Skeleton startColor="gray.300" rounded={50} width={'80%'} h={3} />
+            <Skeleton startColor="gray.300" rounded={50} width={'30%'} h={2} />
+        </VStack>
+        <Avatar
+          bg={colors.trans_primary}
+          width={42}
+          height={42}
+          source={{ uri: null }}
+        />
+      </HStack>
+      <Divider/>
+      <HStack mt={4} mb={3} space={3}>
+        <Text style={{ fontSize: 14 }}>
+          <Skeleton startColor="gray.300" rounded={50} w={100} h={2} />
+        </Text>
+        <Text style={{ fontSize: 14 }}>
+          <Skeleton startColor="gray.300" rounded={50} w={100} h={2} />
+        </Text>
+        <Text style={{ fontSize: 14 }}>
+          <Skeleton startColor="gray.300" rounded={50} w={100} h={2} />
+        </Text>
+      </HStack>
+      <HStack mt={6} space={18} justifyContent={"space-between"}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: colors.transp_danger,
+            height: 40,
+            width: 133,
+            display: "flex",
+            flexDirection: "row",
+            borderRadius: 10,
+            justifyContent: "center",
+            marginRight: 10,
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 16 }}>
+            <Skeleton startColor="gray.300" w={100} h={1} />
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            backgroundColor: colors.trans_primary,
+            height: 40,
+            width: 133,
+            display: "flex",
+            flexDirection: "row",
+            borderRadius: 10,
+            justifyContent: "center",
+            marginRight: 10,
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 16 }}>
+            <Skeleton startColor="gray.300" w={100} h={1} />
+          </Text>
+        </TouchableOpacity>
+      </HStack>
+    </View>
+  );
+};
