72b876489ddc29df627d8e74f8e40259edc34be0
dongmodonald95 <dongmodonald95@gmail.com>
2023-09-01 20:25:00 +0100

    updates: filtrer liste rdv

--- src/screens/MesRdv/index.js
+++ src/screens/MesRdv/index.js
@@ -2,9 +2,7 @@ import React, { useState } from "react";
 import {
   Box,
   Center,
-  FlatList,
   HStack,
-  ScrollView,
   View,
   VStack,
   Actionsheet,
@@ -20,24 +18,41 @@ import { clearCache, getMyRDV } from "../../redux/RDV/actions";
 import { FolderOpen, Sort } from "iconsax-react-native";
 import styles from "./style";
 import colors from "../../constants/colours";
+import { FlatList } from "react-native";
 
 export default function MesRdv({ navigation }) {
-  const [actualState, setActualState] = useState(1);
+  const rdvs = useSelector(state => state.RdvForm.myRdv)
+  const [globalState, setGlobalState] = useState({
+    actualState: 1,
+    loading: false,
+    filterBy: 'create_at',
+    status: 'Planifié',
+    showSkeleton: false,
+    sortOrder: 'asc',
+    appointments: rdvs
+  })
+
   const dispatch = useDispatch()
   const loadingRDV = useSelector(state => state.RdvForm.rdvLoading)
-  const rdvs = useSelector(state => state.RdvForm.myRdv)
   const user = useSelector(state => state.UserReducer.userInfos)
-  const [loading, setLoading] = useState(false);
-  const [status, setStatus] = useState('Planifié')
-  const [appointments, setAppointment] = useState(rdvs)
-  const [showSkeleton, setShowSkeleton] = useState(false)
   const { isOpen, onOpen, onClose } = useDisclose();
+
+  const compareFunction = (a, b) => {
+    const aValue = globalState.filterBy === "created_at" ? a.created_at : a.date;
+    const bValue = globalState.filterBy === "created_at" ? b.created_at : b.date;
+
+    if (globalState.sortOrder === "asc") {
+      return aValue.localeCompare(bValue);
+    } else {
+      return bValue.localeCompare(aValue);
+    }
+  };
 
   const getRdvs = () => {
     dispatch(clearCache())
     if (rdvs.length <= 0) {
       dispatch(getMyRDV(user?.user?._id))
-      setShowSkeleton(true)
+      setGlobalState({ ...globalState, showSkeleton: true })
     }
   }
   React.useLayoutEffect(() => {
@@ -45,19 +60,36 @@ export default function MesRdv({ navigation }) {
   }, []);
 
   React.useEffect(() => {
-    const filteredRdvs = rdvs.filter((rdv) => rdv.status == status)
-    setAppointment(filteredRdvs);
-    setTimeout(() => {
-      setShowSkeleton(false)
-    }, 1000);
-  }, [actualState, rdvs])
+    const filteredRdvs = rdvs.filter((rdv) => rdv.status == globalState.status)
+    setGlobalState({ ...globalState, appointments: filteredRdvs, showSkeleton: false, loading: false });
+  }, [globalState.actualState, rdvs])
+
+  React.useEffect(() => {
+    const copy = [...globalState.appointments]
+    copy.sort(compareFunction);
+
+    setGlobalState({ ...globalState, appointments: copy });
+  }, [globalState.filterBy, globalState.sortOrder]);
+
+  const handleStateChange = (newState) => {
+    if(globalState.actualState === newState) return
+
+    const val = newState === 1 ? "Planifié" : "Terminé";
+    setGlobalState({
+      ...globalState,
+      actualState: newState,
+      loading: true,
+      status: newState !== 3 ? val : "Annulé",
+      showSkeleton: true
+    });
+  };
 
   return (
     <View flex={1} paddingTop={2}>
       <HStack mx={3} my={2} justifyContent={'space-between'} alignItems={'center'}>
         <Text fontWeight={600} fontSize={18}>
           Rendez-vous
-          <Text fontSize={14}> ({appointments.length})</Text>
+          <Text fontSize={14}> ({globalState.appointments.length})</Text>
         </Text>
         <Pressable onPress={onOpen}>
           <HStack alignItems={'center'}>
@@ -90,20 +122,12 @@ export default function MesRdv({ navigation }) {
             width={"33%"}
           >
             <Pressable
-              onPress={() => {
-                setActualState(1);
-                setStatus("Planifié")
-                setShowSkeleton(true)
-                setLoading(true);
-                setTimeout(() => {
-                  setLoading(false);
-                }, 1000);
-              }}
+              onPress={() => { handleStateChange(1) }}
               display={"flex"}
               flexDirection={"row"}
               justifyContent={"center"}
               alignItems={"center"}
-              backgroundColor={actualState === 1 ? "#04B7C9" : "#EEEFF3"}
+              backgroundColor={globalState.actualState === 1 ? "#04B7C9" : "#EEEFF3"}
               height={"100%"}
               width={"90%"}
               borderRadius={10}
@@ -112,7 +136,7 @@ export default function MesRdv({ navigation }) {
                 <Text
                   style={{
                     fontSize: 16,
-                    color: actualState === 1 ? "white" : "#909090",
+                    color: globalState.actualState === 1 ? "white" : "#909090",
                   }}
                 >
                   A venir
@@ -130,20 +154,12 @@ export default function MesRdv({ navigation }) {
             width={"34%"}
           >
             <Pressable
-              onPress={() => {
-                setActualState(2);
-                setStatus("Terminé")
-                setShowSkeleton(true)
-                setLoading(true);
-                setTimeout(() => {
-                  setLoading(false);
-                }, 1000);
-              }}
+              onPress={() => { handleStateChange(2) }}
               display={"flex"}
               flexDirection={"row"}
               justifyContent={"center"}
               alignItems={"center"}
-              backgroundColor={actualState === 2 ? "#04B7C9" : "#EEEFF3"}
+              backgroundColor={globalState.actualState === 2 ? "#04B7C9" : "#EEEFF3"}
               height={"100%"}
               width={"90%"}
               borderRadius={10}
@@ -152,7 +168,7 @@ export default function MesRdv({ navigation }) {
                 <Text
                   style={{
                     fontSize: 16,
-                    color: actualState === 2 ? "white" : "#909090",
+                    color: globalState.actualState === 2 ? "white" : "#909090",
                   }}
                 >
                   Terminé
@@ -169,20 +185,12 @@ export default function MesRdv({ navigation }) {
             justifyContent={"center"}
           >
             <Pressable
-              onPress={() => {
-                setActualState(3)
-                setStatus("Annulé")
-                setShowSkeleton(true)
-                setLoading(true);
-                setTimeout(() => {
-                  setLoading(false);
-                }, 1000);
-              }}
+              onPress={() => { handleStateChange(3) }}
               display={"flex"}
               flexDirection={"row"}
               justifyContent={"center"}
               alignItems={"center"}
-              backgroundColor={actualState === 3 ? "#04B7C9" : "#EEEFF3"}
+              backgroundColor={globalState.actualState === 3 ? "#04B7C9" : "#EEEFF3"}
               height={"100%"}
               width={"90%"}
               borderRadius={10}
@@ -191,7 +199,7 @@ export default function MesRdv({ navigation }) {
                 <Text
                   style={{
                     fontSize: 16,
-                    color: actualState === 3 ? "white" : "#909090",
+                    color: globalState.actualState === 3 ? "white" : "#909090",
                   }}
                 >
                   Annulé
@@ -201,15 +209,16 @@ export default function MesRdv({ navigation }) {
           </View>
         </HStack>
       </Box>
-      {appointments.length !== 0 && !loadingRDV && !loading && !showSkeleton && (
+      {globalState.appointments.length !== 0 && !loadingRDV && !globalState.loading && !globalState.showSkeleton && (
         <FlatList
           onRefresh={getRdvs}
-          refreshing={showSkeleton || loadingRDV}
+          refreshing={globalState.showSkeleton || loadingRDV}
           style={styles.flatList}
-          data={appointments}
+          data={globalState.appointments}
           showsVerticalScrollIndicator={false}
           keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) =>
+          windowSize={1}
+          renderItem={({ item }) => (
             <View
               key={item._id}
               height={195}
@@ -231,10 +240,11 @@ export default function MesRdv({ navigation }) {
             >
               <Rdv _id={item._id} localisation={item?.lieu?.location} navigation={navigation} praticien={item?.name + " " + item.surname} date={item?.date} status={item?.status} startTime={item?.displayedDate} motif={item?.motif} />
             </View>
-          } />
+          )}
+        />
       )}
 
-      {loadingRDV || showSkeleton &&
+      {loadingRDV || globalState.showSkeleton &&
         <VStack padding={4} space={1}>
           <VStack>
             <Skelette />
@@ -248,19 +258,77 @@ export default function MesRdv({ navigation }) {
         </VStack>
       }
 
-      {(appointments.length === 0 && !showSkeleton) &&
+      {(globalState.appointments.length === 0 && !globalState.showSkeleton) &&
         <Center flex={1}>
           <FolderOpen variant="Outline" size={80} color="#a7a7a7" />
-          <Text style={styles.empty}>Vous n'avez aucun rendez-vous {status.toLowerCase()}</Text>
+          <Text style={styles.empty}>Vous n'avez aucun rendez-vous {globalState.status.toLowerCase()}</Text>
         </Center>
       }
 
       <Actionsheet isOpen={isOpen} onClose={onClose}>
         <Actionsheet.Content>
-          <Actionsheet.Item>Option 1</Actionsheet.Item>
-          <Actionsheet.Item>Option 2</Actionsheet.Item>
-          <Actionsheet.Item>Option 3</Actionsheet.Item>
-          <Actionsheet.Item color="red.500">Delete</Actionsheet.Item>
+          <Actionsheet.Item>
+            <Text mb={2} fontSize={16} fontWeight={600}>Options de tri</Text>
+            <HStack justifyContent={'center'} style={styles.btnItem}>
+              <Pressable
+                style={[styles.filterBtn, styles.btnLeftItem, {
+                  backgroundColor: globalState.filterBy === 'created_at' ? colors.trans_primary : "transparent",
+                  borderColor: globalState.filterBy === 'created_at' ? colors.primary : "#c2c2c2",
+                  borderRightWidth: globalState.filterBy === 'created_at' ? 1 : 0,
+                }]}
+                onPress={() => { setGlobalState({ ...globalState, filterBy: 'created_at' }) }}
+              >
+                <Text
+                  color={globalState.filterBy === 'created_at' ? 'primary.500' : colors.text_grey_hint}
+                  textAlign={'center'}>
+                  Date de création
+                </Text>
+              </Pressable>
+              <Pressable
+                style={[styles.filterBtn, styles.btnRightItem, {
+                  backgroundColor: globalState.filterBy === 'rdv_date' ? colors.trans_primary : "transparent",
+                  borderColor: globalState.filterBy === 'rdv_date' ? colors.primary : "#c2c2c2",
+                  borderLeftWidth: globalState.filterBy === 'rdv_date' ? 1 : 0,
+                }]}
+                onPress={() => { setGlobalState({ ...globalState, filterBy: 'rdv_date' }) }}
+              >
+                <Text
+                  color={globalState.filterBy === 'rdv_date' ? 'primary.500' : colors.text_grey_hint}
+                  textAlign={'center'}
+                >
+                  Date du rendez-vous
+                </Text>
+              </Pressable>
+            </HStack>
+          </Actionsheet.Item>
+          <Actionsheet.Item>
+            <Text mb={2} fontSize={16} fontWeight={600}>Ordre</Text>
+            <HStack justifyContent={'center'} style={styles.btnItem}>
+              <Pressable style={[styles.sortBtn, styles.btnLeftItem, {
+                backgroundColor: globalState.sortOrder === 'asc' ? colors.trans_primary : "transparent",
+                borderColor: globalState.sortOrder === 'asc' ? colors.primary : "#c2c2c2",
+                borderRightWidth: globalState.sortOrder === 'asc' ? 1 : 0,
+              }]} onPress={() => { setGlobalState({ ...globalState, sortOrder: 'asc' }) }}>
+                <Text
+                  color={globalState.sortOrder === 'asc' ? 'primary.500' : colors.text_grey_hint}
+                  textAlign={'center'}>
+                  Ascendant
+                </Text>
+              </Pressable>
+              <Pressable style={[styles.sortBtn, styles.btnRightItem, {
+                backgroundColor: globalState.sortOrder === 'desc' ? colors.trans_primary : "transparent",
+                borderColor: globalState.sortOrder === 'desc' ? colors.primary : "#c2c2c2",
+                borderLeftWidth: globalState.sortOrder === 'desc' ? 1 : 0,
+              }]} onPress={() => { setGlobalState({ ...globalState, sortOrder: 'desc' }) }}>
+                <Text
+                  textAlign={'center'}
+                  color={globalState.sortOrder === 'desc' ? 'primary.500' : colors.text_grey_hint}
+                >
+                  Descendant
+                </Text>
+              </Pressable>
+            </HStack>
+          </Actionsheet.Item>
         </Actionsheet.Content>
       </Actionsheet>
     </View>
